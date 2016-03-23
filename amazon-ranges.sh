08ac96b60db5c1159dfd379e1956d44934c56c33
Elliott Eggleston <ejegg@ejegg.com>
2016-03-23 08:31:52 -0700

    Wrap lines

--- amazon-ranges.sh
+++ amazon-ranges.sh
@@ -4,7 +4,9 @@ INCLUDEFILE=`mktemp`
 EXCLUDEFILE=`mktemp`
 MYPATH=`dirname $0`
 wget --quiet https://ip-ranges.amazonaws.com/ip-ranges.json -O $IPFILE
-jq '.prefixes[] | select(.region|startswith("us-east-1")) | select(.service=="AMAZON") | .ip_prefix' $IPFILE | sed -e 's/"//g' > $INCLUDEFILE
-jq '.prefixes[] | select(.service!="AMAZON") | .ip_prefix' $IPFILE | sed -e 's/"//g' > $EXCLUDEFILE
+jq '.prefixes[] | select(.region|startswith("us-east-1")) | select(.service=="AMAZON") | .ip_prefix' $IPFILE \
+    | sed -e 's/"//g' > $INCLUDEFILE
+jq '.prefixes[] | select(.service!="AMAZON") | .ip_prefix' $IPFILE \
+    | sed -e 's/"//g' > $EXCLUDEFILE
 $MYPATH/netdiff.py $INCLUDEFILE $EXCLUDEFILE
 rm $IPFILE $INCLUDEFILE $EXCLUDEFILE
